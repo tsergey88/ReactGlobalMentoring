e04ba202209096a8072b15865e8c0fb068a5ca3d
Sergei Timushev <tsergeyrol08@gmail.com>
2019-03-17 15:22:46 +0300

    task2

---
+++ Task2/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { MainComponent } from "./components/MainComponent";
+
+import "./main.css";
+
+ReactDOM.render(<MainComponent />, document.getElementById("root"));
---
+++ Task2/webpack.common.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new ExtractTextPlugin({ filename: 'style.css' }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader']
+        })
+      }
+    ]
+  },
+};
