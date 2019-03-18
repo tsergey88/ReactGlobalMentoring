d29cf86d3d668337ded59cc4eac9e632f7480a71
Sergei Timushev <tsergeyrol08@gmail.com>
2019-03-19 01:24:07 +0300

    add env variables

--- Task2/webpack.config.js
+++ Task2/webpack.config.js
@@ -1,11 +1,21 @@
 const path = require('path');
+const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Uglify = require('uglifyjs-webpack-plugin');
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const DEV_TOOL = process.env.NODE_ENV === 'development' ? 'inline-source-map' : false;
 
 module.exports = {
+  mode: NODE_ENV,
   entry: {
     app: './src/index.js'
+  },
+  devtool: DEV_TOOL,
+  devServer: {
+    contentBase: './dist'
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -35,6 +45,22 @@ module.exports = {
           use: ['css-loader']
         })
       }
+    ]
+  },
+  optimization: {
+    minimize: NODE_ENV === "production",
+    minimizer: [
+      new webpack.optimize.ModuleConcatenationPlugin(),
+      new Uglify({
+        uglifyOptions: {
+          compress: {
+            drop_console: false,
+            drop_debugger: true
+          },
+          sourceMap: false,
+          warnings: false
+        }
+      })
     ]
   },
 };
