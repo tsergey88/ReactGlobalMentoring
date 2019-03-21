9768d71573b128bf1b9ae6e27798e4aec1e6cd5e
Sergei Timushev <tsergeyrol08@gmail.com>
2019-03-21 23:43:52 +0300

    after code review

--- Task2/webpack.config.js
+++ Task2/webpack.config.js
@@ -1,66 +1,16 @@
 const path = require('path');
-const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const Uglify = require('uglifyjs-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
-const DEV_TOOL = process.env.NODE_ENV === 'development' ? 'inline-source-map' : false;
+const config = NODE_ENV === 'production' ? require('./config/webpack.prod.config') : require('./config/webpack.dev.config.js');
 
 module.exports = {
   mode: NODE_ENV,
   entry: {
     app: './src/index.js'
   },
-  devtool: DEV_TOOL,
-  devServer: {
-    contentBase: './dist'
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new ExtractTextPlugin({ filename: 'style.css' }),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader']
-        })
-      }
-    ]
-  },
-  optimization: {
-    minimize: NODE_ENV === "production",
-    minimizer: [
-      new webpack.optimize.ModuleConcatenationPlugin(),
-      new Uglify({
-        uglifyOptions: {
-          compress: {
-            drop_console: false,
-            drop_debugger: true
-          },
-          sourceMap: false,
-          warnings: false
-        }
-      })
-    ]
-  },
-};
+  ...config
+}
